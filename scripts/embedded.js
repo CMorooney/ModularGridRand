441c5afe310bf1aed9ee0823afe7ece382f8234b
Calvin <calvin@Calvins-MacBook-Pro.local>
2020-12-27 10:55:49 -0500

    Initial Commit   - dummy icon   - dummy pop-up window   - working random button, dummy style

---
+++ scripts/embedded.js
@@ -0,0 +1,64 @@
+var allModulesDocumentLocation = "/e/modules";
+
+//ostensibly this will be called when this script is embedded
+//which should be after document is ready to go
+initialize();
+
+function initialize() {
+    let btn = document.createElement("button");
+    btn.classList.add('fab');
+    btn.onclick = rollTheDie;
+    document.body.appendChild(btn);
+}
+
+function rollTheDie() {
+  //we are requesting the root "all" modules page in order
+  //to be able to read how many 'pages' of modules there are,
+  //which will allow us to generate a random page, then load
+  //page and pick a random module from it.
+  requestDocument(allModulesDocumentLocation,
+                  handleAllModulesPageReturned,
+                  handleAllModulesPageRequestError);
+}
+
+function handleAllModulesPageReturned(page){
+  let totalPages = getTotalPageNumber(page);
+  if(totalPages == -1){
+    handleUnexpectedError("getTotalPageNumber returned -1, cannot generate a random page");
+    return;
+  } 
+
+  let randomPageNumber =  Math.floor(Math.random() * totalPages);
+  let documentUrl = `/e/modules/index/page:${randomPageNumber}`;
+  requestDocument(documentUrl,
+                  handleRandomPageReturned,
+                  function(s, c) { handleRandomPageRequestError(documentUrl, s, c) });
+}
+
+function handleRandomPageReturned(page){
+  let moduleDestination = getRandomModuleLinkFromPage(page);
+  if(moduleDestination){
+    window.location.href = moduleDestination;
+  } else {
+    handleUnexpectedError(`couldn't navigate to module destination: ${moduleDestination}`);
+  }
+}
+
+function handleAllModulesPageRequestError(status, content){
+  handleUnexpectedError(
+    `error requesting document from ${allModulesDocumentLocation}
+    \n${status}\n${content}`
+  );
+}
+
+function handleRandomPageRequestError(url, status, content){
+  handleUnexpectedError(
+    `error requesting random page from ${url}
+    \n${status}\n${content}`
+  );
+}
+
+function handleUnexpectedError(description) {
+  //TODO something real, log GH issue?
+  console.log("*** mgrid random error //:: \n" + description)
+}
